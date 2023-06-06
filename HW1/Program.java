d54acf461988aafc0e6b43a03886a8bffcd3026e
Danila <danchik.b@mail.ru>
2023-06-07 02:07:05 +0300

    HW1 is done

---
+++ HW1/Program.java
@@ -0,0 +1,52 @@
+package HW1;
+
+import java.util.Scanner;
+
+/**
+ * Program
+ */
+public class Program {
+  public static void main(String[] args) {
+    boolean isActive = true;
+
+    while (isActive) {
+      System.out.print("Введите номер задачи, которую хотите проверить(от 1 до 4)\nДля выхода напишите 0: ");
+      Scanner sc = new Scanner(System.in);
+      int num = sc.nextInt();
+      sc.nextLine();
+
+      switch (num) {
+        case 1:
+          System.out.println();
+          Funcs.Task1();
+          break;
+          
+          case 2:
+          System.out.println();
+          Funcs.Task2();
+          break;
+          
+          case 3:
+          System.out.println();
+          Funcs.Task3();
+          break;
+          
+          case 4:
+          System.out.println();
+          Funcs.Task4();
+          break;
+          
+          case 0:
+          System.out.println();
+          isActive = false;
+          break;
+          
+          default:
+          System.out.println();
+          System.out.println("Задача не распознана, повторите!");
+          break;
+      }
+    }
+  }
+
+}
